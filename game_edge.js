3ccd88c3b656281a149f4b941f28071eca6f7f98
Satoshi Ohki <roothybrid7@gmail.com>
2012-08-22 15:34:25 +0900

    Add lifebar.

--- game_edge.js
+++ game_edge.js
@@ -150,12 +150,142 @@ var symbols = {
    gpuAccelerate: false,
    resizeInstances: false,
    content: {
+   dom: [
+   {
+      rect: ['0','0','540','105','undefined','undefined'],
+      id: 'lifeBarArea',
+      stroke: [0,'rgb(0, 0, 0)','none'],
+      type: 'rect',
+      fill: ['rgba(43,218,181,0.90)'],
+      c: [
+      {
+         rect: ['35','40','200','25','undefined','undefined'],
+         borderRadius: ['10px','10px','10px','10px'],
+         transform: {},
+         id: 'enemyLifeBar',
+         stroke: [0,'rgb(0, 0, 0)','none'],
+         type: 'rect',
+         fill: ['rgba(255,0,0,1.00)']
+      },
+      {
+         rect: ['305','39','200','25','undefined','undefined'],
+         borderRadius: ['10px','10px','10px','10px'],
+         transform: {},
+         id: 'playerLifeBar',
+         stroke: [0,'rgb(0, 0, 0)','none'],
+         type: 'rect',
+         fill: ['rgba(255,0,0,1.00)']
+      },
+      {
+         rect: ['35','0px','0','0','undefined','undefined'],
+         transform: {},
+         font: ['Spirax, serif',24,'rgba(0,0,0,1)','normal','none',''],
+         id: 'enemyText',
+         text: 'Enemy',
+         type: 'text',
+         tag: 'p'
+      },
+      {
+         rect: ['435px','0px','0','0','undefined','undefined'],
+         transform: {},
+         font: ['Spirax, serif',24,'rgba(0,0,0,1)','normal','none',''],
+         id: 'playerText',
+         text: 'Player',
+         type: 'text',
+         tag: 'p'
+      },
+      {
+         transform: {},
+         rect: ['35','65px','0','0','undefined','undefined'],
+         font: ['Peralta, serif',24,'rgba(0,0,0,1)','normal','none','normal'],
+         align: 'right',
+         id: 'enemyLife',
+         text: '3000',
+         type: 'text',
+         tag: 'p'
+      },
+      {
+         transform: {},
+         rect: ['435','65px','0','0','undefined','undefined'],
+         font: ['Peralta, serif',24,'rgba(0,0,0,1)','normal','none','normal'],
+         align: 'right',
+         id: 'playerLife',
+         text: '3000',
+         type: 'text',
+         tag: 'p'
+      }]
+   }],
+   symbolInstances: [
+   ]
    },
    states: {
       "Base State": {
+         "${_enemyText}": [
+            ["style", "top", '0px'],
+            ["style", "opacity", '1'],
+            ["style", "left", '35px'],
+            ["style", "font-family", 'Spirax, serif']
+         ],
+         "${_enemyLife}": [
+            ["style", "top", '65px'],
+            ["style", "text-align", 'right'],
+            ["style", "font-family", 'Peralta, serif'],
+            ["style", "opacity", '1'],
+            ["style", "left", '35px'],
+            ["style", "font-size", '24px']
+         ],
+         "${_playerText}": [
+            ["style", "top", '0px'],
+            ["style", "opacity", '1'],
+            ["style", "left", '435px'],
+            ["style", "font-family", 'Spirax, serif']
+         ],
+         "${_playerLife}": [
+            ["style", "top", '65px'],
+            ["style", "text-align", 'right'],
+            ["style", "font-family", 'Peralta, serif'],
+            ["style", "opacity", '1'],
+            ["style", "left", '435px'],
+            ["style", "font-size", '24px']
+         ],
          "${symbolSelector}": [
             ["style", "height", '300px'],
             ["style", "width", '540px']
+         ],
+         "${_enemyLifeBar}": [
+            ["color", "background-color", 'rgba(255,0,0,1)'],
+            ["style", "top", '40px'],
+            ["transform", "scaleY", '1'],
+            ["style", "height", '25px'],
+            ["transform", "scaleX", '1'],
+            ["style", "-webkit-transform-origin", [100,50], {valueTemplate:'@@0@@% @@1@@%'} ],
+            ["style", "-moz-transform-origin", [100,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "-ms-transform-origin", [100,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "msTransformOrigin", [100,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "-o-transform-origin", [100,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "left", '35px'],
+            ["style", "width", '200px']
+         ],
+         "${_playerLifeBar}": [
+            ["color", "background-color", 'rgba(255,0,0,1)'],
+            ["style", "-webkit-transform-origin", [0,50], {valueTemplate:'@@0@@% @@1@@%'} ],
+            ["style", "-moz-transform-origin", [0,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "-ms-transform-origin", [0,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "msTransformOrigin", [0,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["style", "-o-transform-origin", [0,50],{valueTemplate:'@@0@@% @@1@@%'}],
+            ["transform", "scaleY", '1'],
+            ["style", "top", '39.99px'],
+            ["transform", "scaleX", '1'],
+            ["style", "height", '25px'],
+            ["style", "left", '305px'],
+            ["style", "width", '200px']
+         ],
+         "${_lifeBarArea}": [
+            ["color", "background-color", 'rgba(43,218,181,0.8984)'],
+            ["style", "height", '105px'],
+            ["style", "opacity", '1'],
+            ["style", "left", '0px'],
+            ["style", "top", '0px']
          ]
       }
    },
